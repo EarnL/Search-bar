55612ecb7bbba25b5e24f85025a6d6158a397ccc
Arnel Navorra Caparros <arnelcaparros02@gmail.com>
2025-04-30 21:00:50 +0800

    Fix accessibility issue with empty headings in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import BookSearch from './BookSearch'; // Import the new component
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+  
       <BookSearch /> {/* Use the component */}
     </div>
   );
